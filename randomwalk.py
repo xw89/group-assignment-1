b7a7c1a2621e6109219efced65f5cc0da5329191
peifanliu <pl112@duke.edu>
2016-03-17 16:24:22 -0400

    Added files via upload

--- randomwalk.py
+++ randomwalk.py
@@ -5,59 +5,88 @@ Created on Sat Mar 12 15:35:28 2016
 
 """
 from pylab import*
-# an array for random numbers(10000*100)
-random=np.random.rand(10000,100)
-# initialize four array to record the x and y position and x^2, y^2 for the 10000 walkers
-x=np.zeros([10000,101])
-y=np.zeros([10000,101])
-xsq=np.zeros([10000,101])
-ysq=np.zeros([10000,101])
-# four arrays for to record average among walkers of the 100 steps
-xav=np.zeros(101)
-yav=np.zeros(101)
-xsqav=np.zeros(101)
-ysqav=np.zeros(101)
-rsqav=np.zeros(101)
-# loop over 10000 walkers for 100 steps
-for i in range(100): # if random [0,0.25),x+1; if  [0.25,0.5),x-1; if [0.5,0.75), y+1, if  [0.75,1],y-1     
-    for k in range(10000):        
-        if random[k][i]<0.25:
-            x[k][i+1]=x[k][i]+1
-            y[k][i+1]=y[k][i]
-        elif random[k][i]<0.5:
-            x[k][i+1]=x[k][i]-1
-            y[k][i+1]=y[k][i]
-        elif random[k][i]<0.75:
-            x[k][i+1]=x[k][i]
-            y[k][i+1]=y[k][i]+1
-        else:
-            x[k][i+1]=x[k][i]
-            y[k][i+1]=y[k][i]-1
-        xsq[k][i+1]=(x[k][i+1])**2
-        ysq[k][i+1]=(y[k][i+1])**2
-    xav[i+1]=sum(x[:,i+1])/10000
-    yav[i+1]=sum(y[:,i+1])/10000
-    xsqav[i+1]=sum(xsq[:,i+1])/10000
-    ysqav[i+1]=sum(ysq[:,i+1])/10000
-    rsqav[i+1]=xsqav[i+1]+ysqav[i+1]
-"""
-part a, figure for x_n and x_n^2
-"""
-t=range(101)
-fig1=plt.figure()
-plot(t,xav,'gx')
-plot(t,xsqav,'ro')
-xlabel('Steps')
-ylabel('x and x^2')
-legend(['x_n average','x^2_n average'])
-fig1.savefig('C:\\Users\\Xinyu Wu\\Desktop\\566\\my work\\group_assignment_1\\rwxn.pdf') # the path in my computer
+import turtle
 
-"""
-part b, figure for rn^2 
-"""
-fig2=plt.figure()
-plot(t,rsqav,'ro')
-xlabel('Steps')
-ylabel('Distance^2')
-fig2.savefig('C:\\Users\\Xinyu Wu\\Desktop\\566\\my work\\group_assignment_1\\rwrnsq.pdf') # path in my computer
+n = 105			#n steps
+n_walk = 10000		#number of walkers
+
+# initialize four array to record the x and y position and x^2, y^2 for the n_walk walkers
+x = np.zeros([n_walk,n])
+y = np.zeros([n_walk,n])
+xsq = np.zeros([n_walk,n])
+ysq = np.zeros([n_walk,n])
+# four arrays for to record average among walkers of the n steps
+xav = np.zeros(n-1)		#x average
+yav = np.zeros(n-1)		#y average
+xsqav = np.zeros(n-1)		#x^2 average
+ysqav = np.zeros(n-1)		#y^2 average
+rsqav = np.zeros(n-1)		#r^2 average
+
+# loop over n_walk walkers for n steps
+for i in range(n-1): # if random [0,0.25),x+1; if  [0.25,0.5),x-1; if [0.5,0.75), y+1, if  [0.75,1],y-1     
+    for k in range(n_walk): 
+	rd = np.random.rand()       
+        if rd < 0.25:
+            x[k][i+1] = x[k][i]+1	#right motion
+            y[k][i+1] = y[k][i]
+        elif rd < 0.5:
+            x[k][i+1] = x[k][i]-1	#left motion
+            y[k][i+1] = y[k][i]
+        elif rd < 0.75:
+            x[k][i+1] = x[k][i]		#up motion
+            y[k][i+1] = y[k][i]+1
+        else:
+            x[k][i+1] = x[k][i]		#down motion
+            y[k][i+1] = y[k][i]-1
+        xsq[k][i] = (x[k][i])**2
+        ysq[k][i] = (y[k][i])**2
+    xav[i] = sum(x[:,i])/n_walk		
+    yav[i] = sum(y[:,i])/n_walk		
+    xsqav[i] = sum(xsq[:,i])/n_walk	
+    ysqav[i] = sum(ysq[:,i])/n_walk	
+    rsqav[i] = xsqav[i] + ysqav[i]	#r^2 = x^2 + y^2
+
+#part a, figure for x_n and x_n^2
+t = range(n-1)
+fig1 = plt.figure()
+plot(t,xav,'ro', label = '$<x_n>$ average')
+plot(t,xsqav,'gx',label = '$<x^2_n>$ average')
+grid()
+xlabel('Steps(n)')
+ylabel('$<x>$ and $<x^2>$')
+legend(loc=2)
+fig1.savefig('rwxn.pdf') 
+
+#part b, figure for rn^2 
+fig2 = plt.figure()
+plot(t,rsqav,'.r', label = '$<r^2>$')
+grid()
+legend(loc=2)
+xlabel('Steps(n)')
+ylabel('$r^2$')
+fig2.savefig('rwxn2.pdf') 
+
+#Plot a 2D random walk figure 
+fig3 = plt.figure()
+plot(x[1,:],y[1,:])
+title("2D Random Walk")
+xlabel('x')
+ylabel('y')
+fig3.savefig('rwxn3.pdf') 
+show()
+
+#Animition in turtle
+walk = [x[1,:],y[1,:]]
+for x, y in zip(*walk):
+    turtle.goto(x*30,y*30)		#x,y times 30 to make it clear to see each step
+
+turtle.exitonclick()
+
+
+
+
+
+
+
+
 
